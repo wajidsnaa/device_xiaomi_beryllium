814a34d631872bf01088f7676dfecfafb4ba8203
fabianbees <fabianbees@web.de>
2019-03-11 10:56:35 +0100

    beryllium: Update to latest stable Pie fingerprint
    Change-Id: I48e50f2dab89ca41d22418c1e059be704e80132b
    See: https://gitlab.com/LineageOS/issues/android/issues/246

--- havoc_beryllium.mk
+++ havoc_beryllium.mk
@@ -19,10 +19,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=WajidKhan
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.1.3.0.PEJMIFI:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.3.0.PEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.1.3.0.PEJMIFI release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.2.3.0.PEJMIXM release-keys" \
     PRODUCT_NAME="beryllium" \
     TARGET_DEVICE="beryllium"
 
