cfbdeeb72576b06d4e82ab1476f308c725d46bc8
wajidsnaa <wajidsnaakhan@gmail.com>
2019-11-30 08:10:30 +0000

    beryllium: Update fingerprint

--- havoc_beryllium.mk
+++ havoc_beryllium.mk
@@ -19,10 +19,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=Wajid&Tushar
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.3.0.PEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.3.5.0.PEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.2.3.0.PEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.3.5.0.PEJMIXM release-keys" \
     PRODUCT_NAME="beryllium" \
     TARGET_DEVICE="beryllium"
 
